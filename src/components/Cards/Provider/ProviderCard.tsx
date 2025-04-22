6fda860da7bf819610ac23dba2e8fb94c7dcdb69
Tobias Almada <talmada908@gmail.com>
2025-04-22 01:52:22 -0300

    providers card improvement

--- src/components/Cards/Provider/ProviderCard.tsx
+++ src/components/Cards/Provider/ProviderCard.tsx
@@ -82,7 +82,7 @@ export default function ProviderCard({
             </div>
           </div>
 
-          <div className="flex gap-2">
+          <div className="flex flex-wrap gap-2">
             <Button
               onClick={showProvider}
               variant="outline"
